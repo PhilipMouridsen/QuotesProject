d378c635881865752ded8e17120b386f374eab47
Lasse Funder Andersen <lafa@itu.dk>
2020-10-13 09:30:35 +0200

    added multithreading. FAST!

---
+++ QuoteExtractor.py
@@ -0,0 +1,1 @@
+# (\n-\s)(.*)?(?=(, (siger|uddyber|mener|skriver|lød|lyder)))
--- Scraper.py
+++ Scraper.py
@@ -1,21 +1,29 @@
 # scraper
+import time
 import pandas as pd
 import urllib.request
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import concurrent.futures
+MAX_THREADS = 30
+start_time = time.time()
 
-data = pd.read_csv("artikelurls.csv", encoding="utf-16", sep='\t')
+data = pd.read_csv("artikelurls1000.csv", encoding="utf-16", sep='\t', index_col=0)
+result = data.index
+# print ("RESULT", result)
 total = 0
 
-print(data.describe)
-print(data.head)
+# print(data.describe)
+# print(data.head)
 
-print(data.columns)
-data = data.drop(columns=['URN, Pub., Format and 2 more (Combined)','Brugere', 'Gns. tid', 'Afs. video', '% Afs. video','SoMe', '% SoMe', 'Search', '% Search', 'URN Count d' ])
-print(data)
+# print(data.columns)
+data = data.drop(columns=['Brugere', 'Gns. tid', 'Afs. video', '% Afs. video','SoMe', '% SoMe', 'Search', '% Search', 'URN Count d' ])
+# print(data)
+
 
 
 def getHTML(url):
+    # getting the raw HTML from a given url
     global total
     total = total + 1 
     print("fetching article", total)
@@ -26,6 +34,7 @@ def getHTML(url):
 
 
 def get_text_from_HTML(html):
+    # getting the raw text from all p tags with the class dre-article-body__paragraph')  
     soup = BeautifulSoup(html, features="html.parser")
     result = ""
     paragraphs = soup.findAll('p', attrs={'class':'dre-article-body__paragraph'})
@@ -37,17 +46,39 @@ def get_text_from_HTML(html):
 
     return result
 
-data['HTML'] = [getHTML(x) for x in data['URL']]
-data['Text'] = [get_text_from_HTML(x) for x in data['HTML']]
-data = data.drop(columns=['HTML', 'URL'])
 
+def get_text_from_url(id):
+    # print(id)
+    data.loc[id, 'Text'] = get_text_from_HTML(getHTML(data.loc[id, 'URL']))
+    return 
+
+
+threads = min(MAX_THREADS, len(data['URL']))
+data['HTML'] = [None] * len(data['URL'])
+data['Text'] = [None] * len(data['URL'])
+with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+     executor.map(get_text_from_url, data.index)
+     #for t in txt:
+        # print(t)
+     # print(txt.result())
+     # data['Text'] = txt
+    
+print("DONE WITH ALL")
+# print(data)
+
+# data['HTML'] = [getHTML(x) for x in data['URL']]
+# data['Text'] = [get_text_from_HTML(x) for x in data['HTML']]
+# data = data.drop(columns=['HTML'])
+
+print("WRITE TO FILE")
+print('Total time:', time.time()-start_time)
 data.to_csv("test.csv", encoding="utf-16", sep='\t')
 
 # test the file
-test = pd.read_csv('test.csv',encoding="utf-16", sep='\t')
-print(test)
+# test = pd.read_csv('test.csv',encoding="utf-16", sep='\t', index_col = 0)
+# print(test)
 
-# for text in test['Text']:
-#    print(text)
+#for text in test['Text']:
+    # print(text)
 
 # html = urlopen(url).read()
